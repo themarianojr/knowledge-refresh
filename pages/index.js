e56767d236e7ff73da46e772f1b50b2beb43c762
Mariano Siqueira <contato@marianojr.com.br>
2024-12-14 15:01:08 -0300

    alterar mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Marina, papai está se esforçado para se manter capacitado</h1>
+    return <h1>Marina, papai está se esforçado para se manter capacitado ;)</h1>
 }
 
 export default Home;
