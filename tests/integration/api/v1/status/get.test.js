dae85811f9022f9315d751e6718dbd0009562f52
Mariano Siqueira <contato@marianojr.com.br>
2024-12-19 16:45:11 -0300

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
+  //Service Up
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  //UpdateAt definition
+  const responseBody = await response.json();
+
+  //UpdateAt format
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  //PostgresVersion
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  //Max Connections
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  //Opened Connections
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
